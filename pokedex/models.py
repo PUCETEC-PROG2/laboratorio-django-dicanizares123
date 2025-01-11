280008bad012c6fe7367c7abb9efc9931583b0e1
dicanizares123 <davidusa1en1@gmail.com>
2025-01-11 00:59:29 -0500

    laboratorio 9

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Trainer(models.Model):
+    first_name = models.CharField(max_length=30, null=False)
+    last_name = models.CharField(max_length=30, null=False) 
+    birth_date = models.DateField()
+    level = models.IntegerField(default=1) 
+    
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
 
 class Pokemon(models.Model):
     name = models.CharField(max_length=30, null=False)
@@ -15,18 +23,14 @@ class Pokemon(models.Model):
     type = models.CharField(max_length=30, choices=POKEMON_TYPES, null=False)
     weight = models.DecimalField(max_digits=6, decimal_places=4)
     height = models.DecimalField(max_digits=6, decimal_places=4)
-    
+    trainer= models.ForeignKey(Trainer,on_delete=models.SET_NULL, null=True)
+
+
+
     def __str__(self):
         return self.name 
     
-class Trainer(models.Model):
-    first_name = models.CharField(max_length=30, null=False)
-    last_name = models.CharField(max_length=30, null=False) 
-    birth_date = models.DateField()
-    level = models.IntegerField(default=1) 
-    
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+
 
      
     
